950b055b8e877c6e4e599ee7e66adf478dde23ed
Tymon3310 <kopczynski.tymon@gmail.com>
2026-01-20 23:24:36 +0100

    Revert "Add rule-lighting RGB matrix effect with split keyboard support"
    This reverts commit ad77360c6ec7cab344e5145deed81d9d44e04862.

--- quantum/rgb_matrix/rgb_matrix.h
+++ quantum/rgb_matrix/rgb_matrix.h
@@ -25,10 +25,6 @@
 #include "color.h"
 #include "keyboard.h"
 
-#ifdef RULE_LIGHTING_ENABLE
-#include "rule_lighting.h"
-#endif
-
 #ifndef RGB_MATRIX_TIMEOUT
 #    define RGB_MATRIX_TIMEOUT 0
 #endif
