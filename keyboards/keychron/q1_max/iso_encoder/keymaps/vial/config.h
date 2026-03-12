60ece58d7e5b263b8196b09e5e44359b525f00d5
Tymon3310 <kopczynski.tymon@gmail.com>
2026-03-12 21:24:38 +0100

    Add vial keymaps for q1_max and v6_version_2 based on keychron keymap

---
+++ keyboards/keychron/q1_max/iso_encoder/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA8, 0xD5, 0xE2, 0xB7, 0x61, 0x3A, 0x93, 0x41}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
