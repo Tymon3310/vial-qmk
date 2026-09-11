99d313c0f143b2ed8804c8f76442da132d587584
Tymon3310 <kopczynski.tymon@gmail.com>
2026-09-12 01:19:19 +0200

    Port Keychron C100 8K

---
+++ keyboards/keychron/c100_8k/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xCB, 0x09, 0x28, 0x9C, 0xC3, 0xFB, 0x5C, 0x21}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 9 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
